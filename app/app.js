b0c95b8766d2d0838c9ffdce536b3da93270ea64
troyth <troyth@gmail.com>
2014-03-02 17:09:28 -0500

    added server

--- app/app.js
+++ app/app.js
@@ -223,5 +223,7 @@ app.put('/admin*', ensureAuthenticated, routes.put);
 app.delete('/admin*', ensureAuthenticated, routes.delete);
 
 
-//export app for the mail server.js server
-exports.app = app;
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
